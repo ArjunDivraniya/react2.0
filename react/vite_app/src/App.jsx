4179007f981ca9f51d790ed95ef3c02cc6010a66
Arjun Divraniya <arjun.divraniya.cg@email.com>
2024-12-04 14:55:32 +0530

    change

--- react/vite_app/src/App.jsx
+++ react/vite_app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/Earth.png'
+import viteLogo from './assets/Chand.png'
 import './App.css'
 
 function App() {
@@ -15,8 +15,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+       
       </div>
-      <h1>Vite + React</h1>
+      <h1>Moon + Sun</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
